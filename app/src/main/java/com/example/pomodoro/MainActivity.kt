e85225709c1710bd36047a823455f2df958ff9cd
Fynn Bodley-Davies <fynnbd@gmail.com>
2021-03-12 15:18:43 +1300

    first commit

---
+++ app/src/main/java/com/example/pomodoro/MainActivity.kt
@@ -0,0 +1,183 @@
+  package com.example.pomodoro
+
+import android.media.RingtoneManager
+import android.net.Uri
+import android.os.Bundle
+import android.os.CountDownTimer
+import android.view.View
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import com.view.circulartimerview.CircularTimerView
+
+
+  class MainActivity : AppCompatActivity() {
+    var currentCountdown: CountDownTimer? = null
+    var studyMins = 25
+    var breakMins = 5
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+    fun startTimer(view: View)
+    {
+        currentCountdown?.cancel()
+        val progBar = findViewById<CircularTimerView>(R.id.progress_circular)
+        val studyTimeMilli = studyMins * 60 * 1000
+        val breakTimeMilli = breakMins * 60 * 1000
+
+        progBar.setMaxValue(studyTimeMilli.toFloat())
+        currentCountdown = PomodoroClockTimer(
+            studyTimeMilli.toLong(),
+            progBar,
+            100,
+            studyTimeMilli,
+            breakTimeMilli,
+            true,
+            this
+        )
+        setToStudy()
+        (currentCountdown as PomodoroClockTimer).start()
+    }
+
+    fun stopTimer(view: View)
+    {
+        currentCountdown?.cancel()
+        val progBar = findViewById<CircularTimerView>(R.id.progress_circular)
+        progBar.progress = 0F
+        progBar.setText("")
+        progBar.setPrefix("Stopped")
+
+    }
+
+    fun setToStudy()
+    {
+        var progbar = findViewById<CircularTimerView>(R.id.progress_circular)
+        progbar.setPrefix("Time:")
+        progbar.setMaxValue((studyMins * 60 * 1000).toFloat())
+        progbar.setBackgroundColor("#e76130")
+
+        try {
+            val notification: Uri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION)
+            val r = RingtoneManager.getRingtone(applicationContext, notification)
+            r.play()
+        } catch (e: Exception) {
+            e.printStackTrace()
+        }
+    }
+
+    fun setToBreak()
+    {
+        var progbar = findViewById<CircularTimerView>(R.id.progress_circular)
+        progbar.setPrefix("Break:")
+        progbar.setMaxValue((breakMins * 60 * 1000).toFloat())
+        progbar.setBackgroundColor("#3242a8")
+
+        try {
+            val notification: Uri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION)
+            val r = RingtoneManager.getRingtone(applicationContext, notification)
+            r.play()
+        } catch (e: Exception) {
+            e.printStackTrace()
+        }
+    }
+
+    fun incStudy(view: View)
+    {
+        var studyCount = findViewById<ImageView>(R.id.study_up)
+        studyMins = studyMins + 1
+        findViewById<TextView>(R.id.study_text).text = studyMins.toString()
+    }
+
+    fun decStudy(view: View)
+    {
+        if (studyMins > 0)
+        {
+            studyMins = studyMins - 1
+            findViewById<TextView>(R.id.study_text).text = studyMins.toString()
+        }
+    }
+
+    fun incBreak(view: View)
+    {
+        breakMins = breakMins + 1
+        findViewById<TextView>(R.id.break_text).text = breakMins.toString()
+    }
+
+      fun decBreak(view: View)
+      {
+          if(breakMins > 0)
+          {
+              breakMins = breakMins - 1
+              findViewById<TextView>(R.id.break_text).text = breakMins.toString()
+          }
+      }
+
+
+
+}
+
+class PomodoroClockTimer(
+    millisInFuture: Long,
+    targetBar: CircularTimerView,
+    countDownInterval: Long,
+    studyTime: Int,
+    breakTime: Int,
+    study: Boolean,
+    main: MainActivity
+)
+    : CountDownTimer(millisInFuture, countDownInterval)
+{
+    var targetBar = targetBar
+    var totalTime = millisInFuture
+    var studyTime = studyTime
+    var breakTime = breakTime
+    var isStudy = study
+    var root = main
+
+    override fun onTick(millisUntilFinished: Long) {
+        var mins = (millisUntilFinished / 1000)/60
+        var secs = (millisUntilFinished / 1000)%60
+        var time: String = mins.toString()+":"+secs.toString()
+        targetBar.progress = millisUntilFinished.toFloat()
+        targetBar.setText(time)
+    }
+
+    override fun onFinish() {
+        if (isStudy)
+        {
+            println("ababababababa")
+            root.currentCountdown = PomodoroClockTimer(
+                breakTime.toLong(),
+                targetBar,
+                50,
+                studyTime,
+                breakTime,
+                false,
+                root
+            )
+            this.cancel()
+            (root.currentCountdown as PomodoroClockTimer).start()
+            root.setToBreak()
+        }
+        else
+        {
+            println("hello world")
+            root.currentCountdown = PomodoroClockTimer(
+                studyTime.toLong(),
+                targetBar,
+                50,
+                studyTime,
+                breakTime,
+                true,
+                root
+            )
+            (root.currentCountdown as PomodoroClockTimer).start()
+            this.cancel()
+            root.setToStudy()
+        }
+    }
+
+}
